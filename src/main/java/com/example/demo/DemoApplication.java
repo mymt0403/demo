2a2898696224844e46f4a49657c6b7692554173f
mymt0403 <kaneking0703@gmail.com>
2025-04-07 15:43:41 +0900

    added google-maps-services

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,22 @@
 package com.example.demo;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
+	private GeoApiContext context;
+//	BuildConfig conf = new BuildConfig();
 
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	String home() {
 		return "Hello World(⋈◍＞◡＜◍)。✧♡";
 	}
@@ -18,4 +25,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public void MapsController() {
+//		this.context = new GeoApiContext.Builder()
+//				.apiKey(conf.API_KEY)
+//				.build();
+	}
+
+	@GetMapping("/maps")
+	public String geocode(@RequestParam String address) throws Exception {
+		GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
+		return results[0].formattedAddress;
+	}
 }
