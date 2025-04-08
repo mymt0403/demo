88ea75859fd3fedf2cb1af6bc77d1b37aa081dba
mymt0403 <kaneking0703@gmail.com>
2025-04-08 11:48:58 +0900

    implement lombok

--- src/main/java/com/example/demo/BulkyGarbageFacility.java
+++ src/main/java/com/example/demo/BulkyGarbageFacility.java
@@ -1,58 +1,18 @@
 package com.example.demo;
 
+import lombok.Data;
+
+@Data
 public class BulkyGarbageFacility {
-    private int latitude;
-    private int longitude;
+    private float latitude;
+    private float longitude;
     private String prefecture;
     private String facilityName;
 
-    public BulkyGarbageFacility(int latitude, int longitude, String prefecture, String facilityName) {
+    public BulkyGarbageFacility(float latitude, float longitude, String prefecture, String facilityName) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.prefecture = prefecture;
         this.facilityName = facilityName;
-    }
-
-    // Getters and Setters
-    public int getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(int latitude) {
-        this.latitude = latitude;
-    }
-
-    public int getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(int longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getPrefecture() {
-        return prefecture;
-    }
-
-    public void setPrefecture(String prefecture) {
-        this.prefecture = prefecture;
-    }
-
-    public String getFacilityName() {
-        return facilityName;
-    }
-
-    public void setFacilityName(String facilityName) {
-        this.facilityName = facilityName;
-    }
-
-    @Override
-    public String toString() {
-        return "BulkyGarbageFacility{" +
-                "latitude=" + latitude +
-                ", longitude=" + longitude +
-                ", prefecture='" + prefecture + '\'' +
-                ", facilityName='" + facilityName + '\'' +
-                '}';
     }
 }
