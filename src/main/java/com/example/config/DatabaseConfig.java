0293079bb4996b87f979fe12a5f0f60badafbe0a
mymt0403 <kaneking0703@gmail.com>
2025-04-08 12:51:34 +0900

    add h2database config

---
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -0,0 +1,7 @@
+package com.example.config;
+
+public class DatabaseConfig {
+    public static final String JDBC_URL = "jdbc:h2:~/test";
+    public static final String JDBC_USER = "sa";
+    public static final String JDBC_PASSWORD = "";
+}
