c30641e23e664071765bc49ae5b26eebaed111d2
mymt0403 <kaneking0703@gmail.com>
2025-04-23 14:42:38 +0900

    delete unused files

--- src/main/java/com/example/demo/config/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.config;
-
-public class DatabaseConfig {
-    public static final String JDBC_URL = "jdbc:h2:mem:testdb";
-    public static final String JDBC_USER = "sa";
-    public static final String JDBC_PASSWORD = "";
-}
