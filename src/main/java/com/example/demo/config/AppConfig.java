a78afb65587a30ad4bdc1e814959c4031e00086f
mymt0403 <kaneking0703@gmail.com>
2025-05-09 13:05:20 +0900

    delete unnecessary annotation

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.example.demo")
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
