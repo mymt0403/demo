aa33a84db4039f73c262ba01d189df2ed7037b08
mymt0403 <kaneking0703@gmail.com>
2025-05-09 16:23:21 +0900

    delete unnecessary import

--- src/test/java/com/example/demo/service/PrefectureServiceImplTest.java
+++ src/test/java/com/example/demo/service/PrefectureServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
