bf5c67455b9399561dd6725d5c37d79df756dbe6
mymt0403 <kaneking0703@gmail.com>
2025-05-28 16:03:44 +0900

    Revert "add CrossOrigin annotation"
    This reverts commit b5e787d809fe9cc63645218ec4746169099cb8fc.

--- src/main/java/com/example/demo/controller/MapsController.java
+++ src/main/java/com/example/demo/controller/MapsController.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "*")
 public class MapsController {
     @Value("${api.key}")
     private String apiKey;
